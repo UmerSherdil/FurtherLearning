ba920c0f257fc0b220adf118fcc9227eb4e1995d
Umer Sherdil Paracha <ga83wec@mytum.de>
2023-01-22 14:34:54 +0100

    Change task done.

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/1_money_change/change.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,46 @@
-def change(money):
-    # write your code here
+import random
 
-    return money
+
+def change(money: int) -> int:
+    # write your code here
+    coins = 0
+    while money != 0:
+        if money - 10 >= 0:
+            money -= 10
+            coins += 1
+        elif money - 5 >= 0:
+            money -= 5
+            coins += 1
+        else:
+            money -= 1
+            coins += 1
+
+    return coins
+
+
+def change_second(money: int) -> int:
+    return int(money / 10) + int((money % 10) / 5) + money % 5
+
+
+def get_random_n(highest_number: int) -> int:
+    return random.randint(0, highest_number)
+
+
+def perform_stress_test() -> None:
+    while True:
+        n = get_random_n(100234)
+        f1 = change(n)
+        f2 = change_second(n)
+
+        if f1 == f2:
+            print("OK")
+        else:
+            print(n)
+            print(f1, f2)
+            break
 
 
 if __name__ == '__main__':
-    m = int(input())
-    print(change(m))
+    # m = int(input())
+    # print(change_second(m))
+    perform_stress_test()
