e74f070480817fd00eb8c69202c32934078f24e3
Umer Sherdil Paracha <ga83wec@mytum.de>
2023-01-22 16:42:59 +0100

    Remove the bug

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,15 +1,33 @@
 from sys import stdin
+from typing import List
+import numpy as np
 
 
-def optimal_value(capacity, weights, values):
+def optimal_value(capacity: int, weights: List[int], values: List[int]) -> int:
     value = 0.
     # write your code here
+    # Sort the bug for the first example data
+    value_per_unit_weight = sorted([v/w for w, v in zip(weights, values)], reverse=True)
+    value_per_unit_weight_sorted_idx = [value_per_unit_weight.index(item) for item in value_per_unit_weight]
+    sorted_values_and_weights = list(zip(values, weights))
+    sorted_values_and_weights_sorted = [sorted_values_and_weights[i] for i in value_per_unit_weight_sorted_idx]
+    while capacity > 0:
+        for v, w in sorted_values_and_weights_sorted:
+            if w != 0:
+                if capacity < w:
+                    added_weight = capacity
+                    capacity = capacity - added_weight
+                    value = value + added_weight*(v/w)
+                elif capacity >= w:
+                    added_weight = w
+                    capacity = capacity - added_weight
+                    value = value + v
 
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, stdin.read().split()))
+    data = list(map(int, input().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
