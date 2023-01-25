5174a00e19f77f13841367ef394877fcf5ff1b58
Umer Sherdil Paracha <ga83wec@mytum.de>
2023-01-25 11:47:25 +0100

    Started algorithm for car fueling problem.

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,8 +1,21 @@
 from sys import stdin
 
+from typing import List
 
-def min_refills(distance, tank, stops):
+
+def min_refills(distance: int, tank: int, stops: List[int]) -> int:
     # write your code here
+    '''
+    :param distance:
+    :param tank:
+    :param stops:
+    :return:
+    Q1: Need a refill? -> Compare the mileage with the farthest gas station -> If the mileage is more than distance -> No need to refill.
+    Q2: If mileage - (Distance of petrol station + Distance to the next petrol station) > mileage -> If yes -> Need refill, other no.
+    Q3: If the mileage is less than the distance between two conseutive fuel stations before the distance cover has been met -> If yes -> -1 as the distance cannot be conveed even with refueling.
+    Till the distance is not reached or the n
+    '''
+
     return -1
 
 
