e905fc7e524bb7ef5f33b169333f890bb88181ac
Umer Paracha <umer.paracha@gns-mbh.com>
2023-01-26 16:25:53 +0100

    Test the implementation

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -24,50 +24,31 @@ def min_refills(distance: int, tank: int, stops: List[int]) -> int:
     '''
     _stops = [0] + stops + [distance]
     distance_between_stops = [b-a for a, b in zip(stops[:-1], stops[1:])] # what if this list becomes emtpy.
-    # if tank >= stops[-1]:
-    #     return 0
-    # elif tank < max(distance_between_stops):
-    #     return -1
-    # elif tank < stops[0]:
-    #     return -1
-    # else:
-    #     while distance != 0:
-    #         distance_travelled = 0
-    #         i = 0
-    #         for stop in stops:
-    #             if tank - stop >= 0:
-    #                 pass
-    #             else:
-    #                 pass
-    #             distance = distance
-    #
-    #
-    #     return -1
-
-    negative_current_tank = False
-    previous_tank = tank
-    distance_travelled = 0
-    delta_d = 0
-    refuel = 0
-    while distance_travelled < distance:
-        current_tank = previous_tank - delta_d
-        if current_tank < 0:
-            negative_current_tank = True
-            break
-        delta_d = distance_between_stops.pop(0)
-        if current_tank - delta_d < 0:
-            previous_tank = tank
-            refuel += 1
-        else:
-            previous_tank = current_tank
-        distance_travelled += delta_d
-
-    if negative_current_tank:
+    if tank >= stops[-1]:
+        return 0
+    elif tank < max(distance_between_stops):
+        return -1
+    elif tank < stops[0]:
         return -1
     else:
+        previous_tank = tank
+        distance_travelled = 0
+        delta_d = 0
+        refuel = 0
+        while distance_travelled < distance:
+            current_tank = previous_tank - delta_d
+            delta_d = distance_between_stops.pop(0)
+            if current_tank - delta_d < 0:
+                previous_tank = tank
+                refuel += 1
+            else:
+                previous_tank = current_tank
+            distance_travelled += delta_d
+
         return refuel
 
 
 if __name__ == '__main__':
+    # d, m, _, *stops = list(map(int, input().split()))
     d, m, _, *stops = map(int, stdin.read().split())
     print(min_refills(d, m, stops))
