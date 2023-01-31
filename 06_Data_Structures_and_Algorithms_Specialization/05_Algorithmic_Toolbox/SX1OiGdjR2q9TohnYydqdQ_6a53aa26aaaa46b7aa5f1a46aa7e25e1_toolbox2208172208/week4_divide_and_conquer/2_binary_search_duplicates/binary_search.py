2dc0443d201a437d8394c6c410b270ebd1577c1d
Umer Sherdil Paracha <ga83wec@mytum.de>
2023-01-31 18:48:15 +0100

    Binary search with duplicates done.

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.py
@@ -1,6 +1,56 @@
-def binary_search(keys, query):
-    # write your code here
-    pass
+from typing import List
+
+"""
+7
+2 4 4 4 7 7 9
+4
+9 4 5 2
+"""
+
+
+def binary_search(keys: List[int], query: int) -> int:
+    low = 0
+    high = len(keys) - 1
+    indices = [-1]
+
+    while low <= high:
+        m = int((low + high) / 2)
+
+        if keys[m] > query:
+            high = m - 1
+        elif keys[m] < query:
+            low = m + 1
+        else:
+            # Keep searching in lower half.
+            indices.append(m)
+            high = m - 1
+
+    if len(indices) == 1:
+        return indices[0]
+    else:
+        idx = min(indices[1:])
+
+    low = 0
+    high = len(keys) - 1
+    indices = [-1]
+
+    while low <= high:
+        m = int((low + high) / 2)
+
+        if keys[m] > query:
+            high = m - 1
+        elif keys[m] < query:
+            low = m + 1
+        else:
+            # Keep searching in upper half.
+            indices.append(m)
+            low = m + 1
+
+    if len(indices) == 1:
+        return indices[0]
+    else:
+        # Take the minimum of the results of lower and upper half.
+        return min(min(indices[1:]), idx)
 
 
 if __name__ == '__main__':
