bc0579c9e615be38df5df22ac4bf7918eda32927
Umer Sherdil Paracha <ga83wec@mytum.de>
2023-02-05 19:09:32 +0100

    Majority element found by dictionaries.

--- 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week4_divide_and_conquer/3_majority_element/majority_element.py
+++ 06_Data_Structures_and_Algorithms_Specialization/05_Algorithmic_Toolbox/SX1OiGdjR2q9TohnYydqdQ_6a53aa26aaaa46b7aa5f1a46aa7e25e1_toolbox2208172208/week4_divide_and_conquer/3_majority_element/majority_element.py
@@ -1,4 +1,7 @@
-def majority_element_naive(elements):
+from typing import List
+
+# https://www.geeksforgeeks.org/majority-element/
+def majority_element_naive(elements: List[float]) -> int:
     for e in elements:
         if elements.count(e) > len(elements) / 2:
             return 1
@@ -6,8 +9,27 @@ def majority_element_naive(elements):
     return 0
 
 
+def find_majority(arr: List[float], size: int) -> int:
+    m = {}
+    for i in range(size):
+        if arr[i] in m:
+            m[arr[i]] += 1
+        else:
+            m[arr[i]] = 1
+
+    is_majority_present = False
+    for key in m:
+        if m[key] > size / 2:
+            is_majority_present = True
+            break
+    if is_majority_present:
+        return 1
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     input_n = int(input())
     input_elements = list(map(int, input().split()))
     assert len(input_elements) == input_n
-    print(majority_element_naive(input_elements))
+    print(find_majority(input_elements, len(input_elements)))
